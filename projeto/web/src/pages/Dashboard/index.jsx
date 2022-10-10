00326e51c1b5c1887fb2b7752fe24853f069efee
Hugo Silva <42209981+hugossilva2@users.noreply.github.com>
2022-10-10 17:57:19 -0300

    inserindo data

--- projeto/web/src/pages/Dashboard/index.jsx
+++ projeto/web/src/pages/Dashboard/index.jsx
@@ -31,12 +31,42 @@ export const Dashboard =() => (
                 </section>
                 <section id="content" className="container max-w-3xl p-4">
 
+                   <div className="p-4 flex space-x-4 items-center justify-center">
+                        <Icon name="back" className="w-6 text-red-500"/>
+                        <span className="font-bold">24 de novembro</span>
+                        <Icon name="arrowRight" className="w-6 text-red-500" />
+                   </div>
+
+
+                    <div className="space-y-4">
+
                     <Card
                     timeA={{ slug: 'sui'}}
                     TimeB={{ slug: 'cam'}}
                     match={{time: '7:00'}}
-
+                    
                     />
+
+                <Card
+                    timeA={{ slug: 'uru' }}
+                    TimeB={{ slug: 'cor' }}
+                    match={{ time: '10:00' }}
+                    
+                    />
+                <Card
+                    timeA={{ slug: 'por' }}
+                    TimeB={{ slug: 'gan' }}
+                    match={{ time: '13:00' }}
+                    
+                    />
+
+                <Card
+                    timeA={{ slug: 'bra' }}
+                    TimeB={{ slug: 'ser' }}
+                    match={{ time: '16:00' }}
+                    
+                    />
+                    </div>
 
 
                 </section>
