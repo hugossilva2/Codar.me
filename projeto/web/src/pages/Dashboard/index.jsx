8085564118e632ccbbdf72b0e14b3ff637c39edb
Hugo Silva <42209981+hugossilva2@users.noreply.github.com>
2022-10-10 15:36:57 -0300

    Final Aula 02

--- projeto/web/src/pages/Dashboard/index.jsx
+++ projeto/web/src/pages/Dashboard/index.jsx
@@ -1,3 +1,41 @@
+import { Icon } from "../../components"
+
 export const Dashboard =() => (
-    <div>Dashboard</div>
+   <div className="">
+
+
+             <header className="bg-red-500 text-white p-4">
+
+                <div className="container max-w-3xl  flex justify-between">
+
+                <img src="/logo/logo-fundo-vermelho.svg" alt="" className="w-28 md:w-40" />
+                
+                <a href="/profile"> 
+                <Icon name="profile" className="w-10" />                
+                </a>
+
+                </div>
+
+            </header>
+
+            <main>
+
+                <section id="header" className="bg-red-500 text-white p-4">
+                <div className="container max-w-3xl space-y-6">
+
+                   <span>Olá Hugo</span>
+                    <h3 className="text-2xl font-bold">Qual é o seu Palpite?</h3>
+
+                    </div>
+
+                </section>
+
+                <section id="content" className="">
+                    
+
+                </section>
+
+            </main>
+
+   </div>
 )
