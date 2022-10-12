bd53be0d624d9a05ebf5e40dfa4a02b68d34b0c9
Hugo Silva <42209981+hugossilva2@users.noreply.github.com>
2022-10-11 21:29:14 -0300

    config prisma

--- projeto/api/setup.js
+++ projeto/api/setup.js
@@ -1,9 +1,12 @@
 import Koa from 'koa'
 import { router } from './router.js'
+import bobyparser from 'koa-bodyparser'
+import cors from '@koa/cors'
 
 
 export const app = new Koa()
 
-
+app.use(cors())
+app.use(bobyparser())
 app.use(router.routes())
 app.use(router.allowedMethods())
